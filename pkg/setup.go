bd8bc207f359abf564b55fafc86d1c7afd9ec589
heng <awd1232009@gmail.com>
2021-10-04 10:53:09 +0800

    refactor: remove submodule Go-Marlin

--- pkg/setup.go
+++ pkg/setup.go
@@ -3,10 +3,20 @@ package pkg
 import (
 	"crypto/ed25519"
 	"crypto/rand"
+	"crypto/rsa"
+	"log"
 )
 
 // Setup generate the public parameter of ZSK and public key and private key
 // of digital signature scheme
-func Setup() (ed25519.PublicKey, ed25519.PrivateKey, error) {
-	return ed25519.GenerateKey(rand.Reader)
+func Setup() (ed25519.PublicKey, ed25519.PrivateKey, *rsa.PrivateKey) {
+	sk, pk, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		log.Fatalf("Generate digital signature key error: %v\n", err)
+	}
+	esk, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalf("Generate encryption key error: %v\n", err)
+	}
+	return sk, pk, esk
 }
--- test/pkg/data_test.go
+++ test/pkg/data_test.go
@@ -1,6 +1,7 @@
 package pkg_test
 
 import (
+	"math/rand"
 	"testing"
 )
 
@@ -9,18 +10,18 @@ const (
 )
 
 func TestRandomDecode(t *testing.T) {
-	// for i := 0; i < randomDecodeTimes; i++ {
-	// 	pp := pkg.ZskPP{
-	// 		OutputOne: uint(rand.Uint32()),
-	// 		OutputTwo: uint(rand.Uint32()),
-	// 	}
-	// 	buf := pp.ByteEncode()
-	// 	ppDecoded, err := pkg.ByteDecode(buf)
-	// 	if err != nil {
-	// 		t.Log(err)
-	// 	}
-	// 	if ppDecoded.OutputOne != pp.OutputOne || ppDecoded.OutputTwo != pp.OutputTwo {
-	// 		t.Log("Decode error, not equal!")
-	// 	}
-	// }
+	for i := 0; i < randomDecodeTimes; i++ {
+		pp := pkg.ZskPP{
+			OutputOne: uint(rand.Uint32()),
+			OutputTwo: uint(rand.Uint32()),
+		}
+		buf := pp.ByteEncode()
+		ppDecoded, err := pkg.ByteDecode(buf)
+		if err != nil {
+			t.Log(err)
+		}
+		if ppDecoded.OutputOne != pp.OutputOne || ppDecoded.OutputTwo != pp.OutputTwo {
+			t.Log("Decode error, not equal!")
+		}
+	}
 }
